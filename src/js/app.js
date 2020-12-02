385476a4b8c99e5f0c5c6272569dd9245751487d
Silvia <sylwia.barresi@gmail.com>
2020-12-02 02:42:40 +0100

    portfolio

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 100) {
+        $(".title").addClass("active");
+        $('header').addClass("background");
+    } else {
+       $(".title").removeClass("active");
+       $("header").removeClass("background");
+    }
+});
+
+
+  $('#letsee').click(function(e){
+    $(".story").addClass('activeopacity');
+  });
+
+
+    $('.trallala h3').on('click', function(){
+      $(this).siblings('p').addClass('defined').toggle('slow');
+
+    });
+
+    $('#contacts').click(function(e){
+      $(".contacts").addClass('activeright');
+    });
+
+
+});
